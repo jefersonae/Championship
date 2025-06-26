ed0b95982856bccd34bf8f93d801688f9e398ddf
Jeff <jefersonae@hotmail.com.br>
2025-06-26 19:21:51 -0300

    Initial configs

---
+++ src/test/java/ifs/championship/ChampionshipApplicationTests.java
@@ -0,0 +1,13 @@
+package ifs.championship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampionshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
